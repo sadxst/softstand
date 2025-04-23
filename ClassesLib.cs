a8c0e017509a14c8b2d500cc97ac2037399ca1f7
sadxst <dcomp96@gmail.com>
2025-04-23 11:28:45 +0300

    Приклад (бібліотека)

---
+++ ClassesLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lab05Lib
+{
+    public class Town
+    {
+        public string Name; 
+        public string Country; 
+        public string Region; 
+        public int Population; 
+        public double YearIncome; 
+        public double Square; 
+        public bool HasPort;
+        public bool HasAirport;
+        public double YearIncomePerInhabitant
+        {
+            get
+            {
+                return GetYearIncomePerInhabitant();
+            }
+        }
+        public double GetYearIncomePerInhabitant()
+        {
+            return YearIncome / Population;
+        }
+    }
+}
