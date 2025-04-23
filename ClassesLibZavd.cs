9564f561b12ada242fe9863a6144654331c43621
sadxst <dcomp96@gmail.com>
2025-04-23 11:35:46 +0300

    Завдання (бібліотека)

---
+++ ClassesLibZavd.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab05LibZavd
+{
+    public class Telephone
+    {
+        public required string Model { get; set; }
+        public required string Color { get; set; }
+        public required string OperatSystem { get; set; }
+        public int Ram { get; set; }
+        public int Cores { get; set; }
+        public int Memory { get; set; }
+        public int Battery { get; set; }
+        public bool HasNfc { get; set; }
+        public bool HasSpeedCharge { get; set; }
+    }
+}
